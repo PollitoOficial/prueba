27d76770a5ed94c15d97b88d0e50cde0b8ea5847
PollitoOficial <emiliojesuscs16@gmail.com>
2025-07-03 11:38:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: []
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que Vite construya en la carpeta correcta
+  },
 })
